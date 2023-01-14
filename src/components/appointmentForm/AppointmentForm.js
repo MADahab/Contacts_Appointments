b01e207f7f1a472c37c439b573f0b1b799465367
MReaper01 <120942366+MReaper01@users.noreply.github.com>
2023-01-14 07:09:21 +0200

    first-working-commit

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -13,14 +13,13 @@ export const AppointmentForm = ({
   setTime,
   handleSubmit
 }) => {
-  const getContactNames = () => {
-    return contacts.map((contact) => contact.name);
-  };
-
-  const handleTitle = ({ target }) => setTitle(target.value);
   
+
+  const handleTitle = ({ target }) => setTitle(target.value);  
   const handleDate = ({ target }) => setDate(target.value);
   const handleTime = ({ target }) => setTime(target.value);
+  const handleContact = ({ target }) => setContact(target.value);
+
 
 
   const getTodayString = () => {
@@ -35,43 +34,50 @@ export const AppointmentForm = ({
   return (
    <form onSubmit={handleSubmit}>
       <label for='title' >Appointment</label>
-      <input 
-        name='title'
-        type='text'
-        value={title} 
-        onChange={handleTitle}   
-        required
-        placeholder="Appointment"  
-        />
+        <input 
+          name='title'
+          type='text'
+          value={title} 
+          onChange={handleTitle}   
+          required
+          placeholder="Appointment"  
+          />
       <br/>
       <label for='contact' >Pick Contact</label>
-      <ContactPicker
-        name="contact"
-        value={contact}
-        contact={contact}
-        contacts={contacts}        
-        placeholder="Appointment With"
-      />
+        <select onChange={handleContact} name="contact" value={contact} >
+          <option value="" selected disabled hidden>no contact is selected</option>
+          {contacts.map((contact,index) => {
+            return (
+              <option 
+                value={contact.name} 
+                key={index}
+                contact={contact.name}
+                >
+                {contact.name}
+              </option>
+            );
+          })}         
+        </select>
       <br/>
       <label for='date' >Assign Date</label>
-      <input
-          type="date"
-          name="date"
-          min={getTodayString()}
-          value={date}
-          onChange={handleDate}
-          required
-          placeholder={getTodayString()}
-      />
+        <input
+            type="date"
+            name="date"
+            min={getTodayString()}
+            value={date}
+            onChange={handleDate}
+            required
+            placeholder={getTodayString()}
+        />
       <br/>
       <label for='time' >Assign Time</label>
-      <input
-          type="time"
-          name="time"
-          value={time}
-          onChange={handleTime}
-          required
-      />
+        <input
+            type="time"
+            name="time"
+            value={time}
+            onChange={handleTime}
+            required
+        />
       <br/>
       <input type="submit" value="Submit" />
    </form>
--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,24 +1,26 @@
 import React from "react";
 
-export const ContactPicker = (contacts, handleContact) => {
-
-  const thelist = () => {contacts.map((contact,index) => {
-    return (
-      <option value={contact.name} key={index}>
-        {contact.name}
-      </option>
-    );
-  })}
+{/* export const ContactPicker = ({contacts, setContact}) => {
 
   
+  
   return (
-    <select onChange={handleContact}>
+    <select onChange={handleContact} name="contact" >
       <option value="" selected disabled hidden>no contact is selected</option>
-      
-      {thelist}
-      
+      {contacts.map((contact,index) => {
+        return (
+          <option 
+            value={contact.name} 
+            key={index}
+            contact={contact.name}
+            >
+            {contact.name}
+          </option>
+        );
+      })}         
     </select>
   );
 };
+*/}
 
 
